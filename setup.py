d1efe7e8564ef7c80175e27a5ea0ef7a8a1fd7e8
Mattia <mattiafailla@hotmail.it>
2020-02-26 23:33:21 +0100

    Code reformat, idea ignored

--- setup.py
+++ setup.py
@@ -3,19 +3,19 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open('requirements.txt') as fp:
+with open("requirements.txt") as fp:
     install_requires = fp.read()
 
 conf = {
-    'name':                             'tentalog',
-    'version':                          '0.1.0',
-    'description':                      'Utility package for logging in Python',
-    'author':                           'NextBlu',
-    'author_email':                     'hello@nextblu.com',
-    'long_description':                 long_description,
-    'long_description_content_type':    'text/markdown',
-    'license':                          'MIT',
-    'packages':                         setuptools.find_packages(),
-    'url':                              'https://github.com/nextblu/tentalog',
-    'install_requires':                 install_requires
+    "name": "tentalog",
+    "version": "0.1.0",
+    "description": "Utility package for logging in Python",
+    "author": "NextBlu",
+    "author_email": "hello@nextblu.com",
+    "long_description": long_description,
+    "long_description_content_type": "text/markdown",
+    "license": "MIT",
+    "packages": setuptools.find_packages(),
+    "url": "https://github.com/nextblu/tentalog",
+    "install_requires": install_requires,
 }
