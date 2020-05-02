9451acea5dd35a2216018bcd375835d79efb64cc
Christian Cavuti <christian.cavuti@gmail.com>
2020-05-02 19:49:50 +0200

    Added BatchHTTPHandler to send logs in batches.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as fp:
 
 setuptools.setup(
     name='tentalog',
-    version='0.1.0',
+    version='0.2.0',
     description='Utility package for logging in Python',
     author='NextBlu',
     author_email='hello@nextblu.com',
--- tentalog/__init__.py
+++ tentalog/__init__.py
@@ -3,6 +3,8 @@ import logging.config
 import logging
 import coloredlogs
 import yaml
+import requests
+from queue import Queue
 
 
 class Tentacle:
@@ -45,7 +47,7 @@ class Tentacle:
     @property
     def name(self):
         return self.__name
-    
+
     @property
     def path(self):
         return self.__path
@@ -53,3 +55,63 @@ class Tentacle:
     @property
     def logger(self):
         return self.__logger
+
+
+class BatchHTTPHandler(logging.handlers.HTTPHandler):
+    """
+    Specialization of the logging.handlers.HTTPHandler that sends requests as JSON and in batch.
+    The payload sent by this handler
+    will look like
+    {
+        'data': [
+            {
+                ...
+            },
+            {
+                ...
+            },
+            ...
+        ]
+    }
+    """
+
+    def __init__(self, host, url, batch_size=10, method='GET', secure=False, credentials=None, context=None):
+        super().__init__(host, url, method=method, secure=secure, credentials=credentials, context=context)
+        self.__host = host
+        self.__url = url
+        self.__batch_size = batch_size
+        self.__queue = Queue()
+        self.__client = requests.Session()
+        self.__client.headers.update({'Content-Type': 'application/json'})
+
+    def emit(self, record):
+        try:
+            self.__queue.put(record)
+            if self.__queue.qsize() > 0 and (self.__queue.qsize() % self.__batch_size) == 0:
+                payload = {}
+                logs = []
+                backup_queue = []
+                i = 0
+                while not self.__queue.empty():
+                    backup_queue.append(self.__queue.get())
+                    logs.append(backup_queue[i].__dict__)
+                    i += 1
+                payload['data'] = logs
+                try:
+                    self.custom_emit(payload)
+                    with self.__queue.mutex:
+                        self.__queue.queue.clear()
+
+                    self.flush()
+                except:
+                    for log in backup_queue:
+                        self.__queue.put(log)
+
+        except:
+            self.handleError(record)
+
+    def custom_emit(self, logs):
+        self.__client.post(self.__host + self.__url, json=logs)
+
+    def __del__(self):
+        self.__client.close()
