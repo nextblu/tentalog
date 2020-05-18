1b286a460f0a66698a49898bdaae3f303e9bfc19
Mattia <mattiafailla@hotmail.it>
2020-05-18 15:56:17 +0200

    Improving test

--- tentalog/test_configuration.py
+++ tentalog/test_configuration.py
@@ -7,14 +7,3 @@ def test_debug_level():
     tentacle = Tentacle()
     logger = tentacle.logger
     assert logger.level, logging.DEBUG
-
-
-def test_configuration_import():
-    tentacle = Tentacle()
-    assert (tentacle.path, "/home/runner/work/tentalog/tentalog/default_configuration.yaml")
-
-
-def test_test():
-    tentacle = Tentacle()
-    log = tentacle.logger
-    assert (True, isinstance(log, Tentacle))
