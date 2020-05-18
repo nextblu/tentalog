98958460a874a20b792f4ff0c289fc2a88a5f120
Mattia <mattiafailla@hotmail.it>
2020-05-18 15:19:16 +0200

    Base test configuration

--- tentalog/test_configuration.py
+++ tentalog/test_configuration.py
@@ -1,8 +1,13 @@
+# content of test_configuration.py
 from tentalog import Tentacle
 import logging
 
 
-def Test_level():
+def test_configuration_import():
+    pass
+
+
+def test_debug_level():
     tentacle = Tentacle()
     logger = tentacle.logger
     assert logger.level, logging.DEBUG
