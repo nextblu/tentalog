639235a862512df6d422bc7600ddc1e222907fe3
Christian Cavuti <c.cavuti@dataone.it>
2020-09-24 17:34:55 +0200

    Update setup.py to change version
    The version in the setup.py has been modified to 0.2.2 to be ready for the next release.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as fp:
 
 setuptools.setup(
     name='tentalog',
-    version='0.2.1',
+    version='0.2.2',
     description='Utility package for logging in Python',
     author='NextBlu',
     author_email='hello@nextblu.com',
