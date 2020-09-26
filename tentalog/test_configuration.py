ccdd92fbdfc9c47579cee3dbdc598ad266362e27
Mattia <mattiafailla@hotmail.it>
2020-09-26 16:00:50 +0200

    Improving testing phase

--- tentalog/test_configuration.py
+++ tentalog/test_configuration.py
@@ -1,7 +1,8 @@
 # content of test_configuration.py
-from .tentacle import Tentacle
-from .batch_http_handler import BatchHTTPHandler
 import logging
+
+from .batch_http_handler import BatchHTTPHandler
+from .tentacle import Tentacle
 
 
 def test_debug_level():
@@ -9,9 +10,17 @@ def test_debug_level():
     logger = tentacle.logger
     assert logger.level, logging.DEBUG
 
+
 def test_batch_handler():
     tentacle = Tentacle().logger
-    handl = BatchHTTPHandler(host='http://localhost:8080', url='/log', batch_size=1)
-    tentacle.addHandler(handl)
+    handler = BatchHTTPHandler(host='http://localhost:8080', url='/log', batch_size=1)
+    tentacle.addHandler(handler)
     tentacle.debug("Hello, World!")
     assert tentacle.level, logging.DEBUG
+
+
+def test_custom_configuration():
+    tentacle = Tentacle(path='./default_configuration.yaml', name='test_logger')
+    logger = tentacle.logger
+    logger.info("Custom configured!")
+    assert tentacle.name, 'test_logger'
